9ac0e44cc71c1496064a9ee09bd50b85320e2979
hechangping <hechangping>
2017-04-19 17:11:21 +0800

    移除部分测试代码

--- app/src/test/java/com/echoleaf/frame/ExampleUnitTest.java
+++ app/src/test/java/com/echoleaf/frame/ExampleUnitTest.java
@@ -1,11 +1,6 @@
 package com.echoleaf.frame;
 
-import com.echoleaf.frame.recyle.TrashMonitor;
-
 import org.junit.Test;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -15,39 +10,6 @@ import java.lang.reflect.Constructor;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        TestC testC = new TestC("1");
-        getB(testC);
     }
 
-
-    private static void getB(Object target) {
-        Constructor<?>[] fields = target.getClass().getConstructors();
-        for (Constructor field : fields) {
-            field.setAccessible(true);
-            Annotation annotation = field.getAnnotation(TrashMonitor.class);
-            if (annotation instanceof TrashMonitor) {
-                TrashMonitor trashMonitor = (TrashMonitor) annotation;
-                System.out.print(trashMonitor);
-            }
-        }
-    }
-
-
-    static class TestC {
-        @TrashMonitor
-        String s;
-
-        public TestC(String s) {
-            this.s = s;
-        }
-
-        @TrashMonitor
-        public TestC(String s, Integer d) {
-            this.s = s;
-        }
-
-        public String getS() {
-            return s;
-        }
-    }
 }
